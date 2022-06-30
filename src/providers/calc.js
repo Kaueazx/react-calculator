3d1982d7d48ef36f56593795eb389036c59d9671
Kaueazx <kaueaexzplosion@hotmail.com>
2022-06-30 11:06:49 -0300

    funções de calculo

--- src/providers/calc.js
+++ src/providers/calc.js
@@ -6,30 +6,57 @@ export function CalcProvider({ children }) {
     const [value, setValue] = useState("0");
 
     function changeValue(newValue) {
-       if (value.includes('/') || 
-           value.includes('+') ||
-           value.includes('-') ||
-           value.includes('*')) {
-        setValue(value + newValue);
-       }
-       else {
-           if (value != "0" && value.length >= 1) {
-            setValue(value + newValue);
-           }
-            else { 
-                setValue(newValue + '');
-           }
-       }
+        if (value.includes('/') ||
+            value.includes('+') ||
+            value.includes('-') ||
+            value.includes('*')) {
+            setValue(value + ' ' + newValue);
+        }
+        else {
+            if (value != "0" && value.length >= 1) {
+                setValue(value + ' ' + newValue);
+            }
+            else {
+                setValue(newValue + ' ');
+            }
+        }
     }
 
     function clean() {
         setValue('0');
     }
 
-    function Calc(){
-        let parts = value.split("+");
-    
-        setValue(parseInt(parts[0]) + parseInt(parts[1]) + '');
+    function Sum(a, b) {
+        return parseInt(a) + parseInt(b)
+    }
+
+    function Sub(a, b) {
+        return parseInt(a) - parseInt(b)
+    }
+
+    function Calc() {
+        let parts = value.split(" ");
+        parts = parts.join('').split('');
+
+        let auxResult = '';
+
+        parts.map((part, index) => {
+            //  console.log(`posição: ${index}\nConteudo`);
+            if (part == '+') {
+              //  auxResult = Sum(index - 1, index + 1);
+              auxResult = (Sum(parts[index - 1], parts[index + 1]));
+            }
+           
+
+        });
+
+        console.log(auxResult);
+        // console.log(parts);
+
+
+
+
+        //  setValue((Sum(parts[0], parts[1])) + '');
     }
 
     function operation(operator) {
@@ -41,9 +68,9 @@ export function CalcProvider({ children }) {
         else {
             // alert(operator);
             // let addValue = value + operator;
-            setValue(value + operator);
+            setValue(value + ' ' + operator);
         }
-        
+
     }
 
     return (
